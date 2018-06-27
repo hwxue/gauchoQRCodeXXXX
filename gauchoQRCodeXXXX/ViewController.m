cc8bd074e9d651873236e92b6ee25425d5b08401
xhxue <2609386917@qq.com>
2018-06-27 16:24:03 +0800

    Initial Commit

---
+++ gauchoQRCodeXXXX/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gauchoQRCodeXXXX
+//
+//  Created by gaucho on 2018/6/27.
+//  Copyright © 2018年 gaucho. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
