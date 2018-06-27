4ebe234d13cbd700d82b720961af169cd434e94f
xhxue <2609386917@qq.com>
2018-06-27 16:27:32 +0800

    first commit

---
+++ gauchoQRCodeXXXX/HWQRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HWQRCodeViewController.h
+//  XHWQRCode
+//
+//  Created by gaucho on 2018/6/27.
+//  Copyright © 2018年 gaucho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HWQRCodeViewController : UIViewController
+
+@end
